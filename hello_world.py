3ff9a823ce9d91c7a7e50da187db62f0974ec743
tinotenda-gufu123 <gufutinotenda@gmail.com>
2024-10-09 00:50:48 +0200

    Initial commit

---
+++ hello_world.py
@@ -0,0 +1,23 @@
+'''
+Pseudo code: 
+
+Request the user to input their name 
+Store the input in a variable called name_of_user 
+Print variable name_of_user 
+
+Request the user to input their age 
+Store the input in a variable called age_of_user 
+Print the variable age_of_user 
+
+Print Hello to the World
+'''
+# Requesting name for output
+name_of_user = input("Please enter your name ")
+print(f"Your name is {name_of_user}.")
+
+# Requesting age for output 
+age_of_user = input("Please enter your age ")
+print(f"You are {age_of_user} year's old.")
+
+# Printing Hello to the World
+print("Hello World. ")
---
+++ menu.py
@@ -0,0 +1,28 @@
+'''
+Pseudo 
+
+Request the user to input 1 of their favourite food
+Store the input in variable item_1 
+
+Request the user to input 1 of their favourite food
+Store the input in variable item_2 
+
+Request the user to input 1 of their favourite food 
+Store the input in variable item_3
+
+Print "Order confirmation! You have ordered: "
+Print item_1
+Print item_2
+Print item_3
+'''
+
+# Requesting the customers favourite foods for order creation
+item_1 = input("Please enter your favourite food ")
+item_2 = input("Please enter your favourite food ")
+item_3 = input("Please enter your favourite food ")
+
+# Printing the order
+print("Order confirmation! You have ordered: ")
+print(item_1)
+print(item_2)
+print(item_3)
